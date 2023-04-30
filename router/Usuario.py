2dd1cb7c7ea11f3901de357c5d1afe57d84c23f5
Camilo-Caldas <juan.1701812099@ucaldas.edu.co>
2023-04-30 16:47:25 -0500

    Pruebas sqlalchemy

--- router/Usuario.py
+++ router/Usuario.py
@@ -6,6 +6,11 @@ from fastapi import APIRouter, HTTPException
 from schema.Usuario import Usuario
 from utils.db import DataBaseConnection, run_query
 
+#--------
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 conn = DataBaseConnection()
 
 
@@ -14,9 +19,33 @@ user = APIRouter()
 
 @user.get("/all/")
 def listar_allUsers():
+    """
     cur = run_query("SELECT * FROM Usuario")
     for data in cur:
-        print(data)
+        print(data)   """
+    engine = create_engine('postgresql://postgres:1234@localhost:5432/HospitalizacionenCasa')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print(session)
+
+    Base = declarative_base()
+
+    class Usuario(Base):
+        __tablename__ = 'usuario'
+        id = Column(Integer, primary_key=True)
+        nombre = Column(String)
+        apellido = Column(String)
+        cedula = Column(Integer)
+        edad = Column(Integer)
+        telefono = Column(String)
+        email = Column(String)
+        password = Column(String)
+        direccion = Column(String)
+
+    usuarios = session.query(Usuario).all()
+
+    for usuario in usuarios:
+        print(usuario.nombre, usuario.apellido,usuario.email, usuario.telefono)
 
 
 @user.post("/usuarios/{id}")
@@ -29,4 +58,8 @@ def actualizar_usuario(id: int, Usuario_Actualizar: Usuario):
         return {"id":id, **Usuario_Actualizar.dict()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
 
